867e157b1f0c845351843f66adcc2559edc722fc
stark <stark.karl.program@gmail.com>
2022-06-01 16:17:33 +0800

    hot-fix commit1

--- src/main/java/com/test/giit/GitTest.java
+++ src/main/java/com/test/giit/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("Hello world");
         System.out.println("Hello world2");
         System.out.println("Hello world3");
+        System.out.println("Hello world4");
     }
 }
